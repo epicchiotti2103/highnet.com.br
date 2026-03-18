9a4ba93111d00413d3a63091d5b90d9cd83167b1
Elio Picchiotti <eliosp20048@gmail.com>
2026-03-18 20:51:17 -0300

    feat: site highnet.com.br completo

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+/* ── PostHog Analytics ──
+   Substitua 'YOUR_POSTHOG_KEY' pela chave do seu projeto PostHog.
+   Crie um projeto em https://app.posthog.com → Settings → Project API Key
+*/
+!function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags getFeatureFlag getFeatureFlagPayload reloadFeatureFlags group updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures getActiveMatchingSurveys getSurveys".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
+
+posthog.init('YOUR_POSTHOG_KEY', {
+  api_host: 'https://app.posthog.com',
+  capture_pageview: true,
+  capture_pageleave: true,
+});
+
+/* ── Mobile nav toggle ── */
+document.addEventListener('DOMContentLoaded', function() {
+  var toggle = document.querySelector('.nav-toggle');
+  var nav = document.querySelector('nav');
+  if (toggle && nav) {
+    toggle.addEventListener('click', function() {
+      nav.classList.toggle('open');
+    });
+  }
+});
